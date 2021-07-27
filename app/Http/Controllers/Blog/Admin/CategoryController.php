c34810b6a9008fffe6a7066fe66191e80b326d6a
Dmitry <dimabaran96@gmail.com>
2021-07-27 12:07:23 +0300

    less_23_create_categories

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
 
@@ -26,7 +27,11 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
@@ -35,9 +40,24 @@ class CategoryController extends BaseController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        dd(__METHOD__);
+        $data = $request->input();
+        if (empty($data['slug'])) {
+            $data['slug'] = str_slug($data['title']);
+        }
+
+        $item = new BlogCategory($data);
+        $item->save();
+
+        if ($item) {
+            return redirect()->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+
     }
 
     /**
@@ -75,16 +95,6 @@ class CategoryController extends BaseController
      */
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-//        $rules = [
-//            'title'       => 'required|min:5|max:200',
-//            'slug'        => 'max:200',
-//            'description' => 'string|max:500|min:3',
-//            'parent_id'   => 'required|integer|exists:blog_categories,id',
-//        ];
-
-//        $validateData = $this->validate($request, $rules);
-//        $validateData = $request->validate($rules);
-
         $item = BlogCategory::find($id);
         if (empty($item)) {
             return back()
@@ -93,6 +103,11 @@ class CategoryController extends BaseController
         }
 
         $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = str_slug($data['title']);
+        }
+
         $result = $item->fill($data)->save();
 
         if ($result) {
--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -4,9 +4,14 @@
     @php
         /** @var \App\Models\BlogCategory $item */
     @endphp
-    <form method="POST" action="{{route('blog.admin.categories.update', $item->id)}}">
+    @if($item->exists)
+        <form method="POST" action="{{route('blog.admin.categories.update', $item->id)}}">
         @method('PATCH')
+    @else
+        <form method="POST" action="{{route('blog.admin.categories.store')}}">
+    @endif
         @csrf
+
         <div class="container">
             @php
                 /** @var \Illuminate\Support\ViewErrorBag $errors */
